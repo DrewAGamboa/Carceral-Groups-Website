a7024ec02f1ce58a408dd30d8ad780f7655b916f
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-04-17 01:50:57 +0000

    add snapshot test

--- my-react-leaflet-app/src/App.test.tsx
+++ my-react-leaflet-app/src/App.test.tsx
@@ -1,3 +1,9 @@
-test('passing test', () => {
-  
-});
+import App from './App';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+    const tree = renderer
+      .create(<App/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
