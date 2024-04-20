5febba8e3704303bf2eb4f534c21fc40824def03
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-04-20 04:05:50 +0000

    add a form to dialog with name, email, and comment

--- carceral-groups-website/src/components/MaterialUI/DocumentDialog.tsx
+++ carceral-groups-website/src/components/MaterialUI/DocumentDialog.tsx
@@ -35,7 +35,7 @@ export default function DocumentDialog(props: DocumentDialogProps) {
     const doc = getDocument(document_id);
     if (doc) {
       // TODO: replace this when files are available
-      setDoc({...doc, fileUrl: DUMMY_BLOBDOCUMENT.fileUrl}); 
+      setDoc({ ...doc, fileUrl: DUMMY_BLOBDOCUMENT.fileUrl });
     } else {
       setDoc(null);
     }
@@ -57,6 +57,17 @@ export default function DocumentDialog(props: DocumentDialogProps) {
       <Dialog
         open={open}
         onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries((formData as any).entries());
+            // const email = formJson.email;
+            console.log(formJson);
+            // handleClose();
+          },
+        }}
       >
         {doc === null &&
           <>
@@ -80,14 +91,37 @@ export default function DocumentDialog(props: DocumentDialogProps) {
                 To leave a comment for this archival material, please enter your comment here. Your comment will be stored for future reference.
               </DialogContentText>
               <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                name="name"
+                label="Name"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="emailAddress"
+                name="email"
+                label="Email Address"
+                type="email"
+                fullWidth
+                variant="standard"
+              />
+              <TextField
+                autoFocus
+                margin='dense'
                 id="outlined-multiline-static"
+                name="comment"
                 label="Multiline"
                 multiline
                 fullWidth
                 rows={4}
                 defaultValue=""
               />
-              <CommentSection document_id={doc.id}/>
+              <CommentSection document_id={doc.id} />
             </DialogContent>
             <DialogActions>
               <Button onClick={handleClose}>Cancel</Button>
