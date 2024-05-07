ec52570ab6e4e5a029a750d5e43742d14259bada
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-05-07 10:56:13 +0000

    Add initial msal packages and instance initialize

--- carceral-groups-website/src/main.tsx
+++ carceral-groups-website/src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>,
 )
