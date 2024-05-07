a13f2b6f0c27b566c026b11a30f588ff7c455e8b
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-05-07 11:27:23 +0000

    Add react router initial setup.

--- carceral-groups-website/src/main.tsx
+++ carceral-groups-website/src/main.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
+
+
 
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from './authConfig';
+import ErrorPage from './error-page.tsx';
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-      <App />
+      <RouterProvider router={router} />
     </MsalProvider>
   </React.StrictMode>,
 )
