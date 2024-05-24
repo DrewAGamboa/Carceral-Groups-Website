1e72b3bb448a31e5961558fb41db49460dcd6cd1
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-05-24 14:34:35 +0000

    refactor to better follow flow of using a backend api

--- carceral-groups-website/src/hooks/useLeafletMap.ts
+++ carceral-groups-website/src/hooks/useLeafletMap.ts
@@ -6,54 +6,73 @@ import Filter from "../models/Filter";
 import GeographicLocationFilter from "../models/GeographicLocationFilter";
 
 const useLeafletMap = () => {
-    const [selectedMarker, setSelectedMarker] = useState<MapPoint[]>()
-    const [dataGeoJson, setDataGeoJson] = useState<any[]>([])
-    const [treeData, setTreeData] = useState<Filter[]>([]);
-    const [selectedGeographicLocationFilters, setSelectedGeographicLocationFilter] = useState<GeographicLocationFilter[]>([]);
-    const [documents, setDocuments] = useState<MapPoint[]>([])
+  const [selectedMarker, setSelectedMarker] = useState<MapPoint[]>()
+  const [dataGeoJson, setDataGeoJson] = useState<any[]>([])
+  const [treeData, setTreeData] = useState<Filter[]>([]);
+  const [selectedGeographicLocationFilters, setSelectedGeographicLocationFilter] = useState<GeographicLocationFilter[]>([]);
+  const [documents, setDocuments] = useState<MapPoint[]>([])
 
-    const handleSelectedInstitutions = (geographicLocationFilters: GeographicLocationFilter[]) => {
-        console.log('Updated Tree Data:', geographicLocationFilters)
-        setSelectedGeographicLocationFilter(geographicLocationFilters)
-    
-        // update geojson show on map
-        // TODO: this should be handled in a use effect to call the api
-        const newGeoJson = dataGeoJson.map((feature) => {
-          const show_on_map = geographicLocationFilters.find((option) => option.Institution === feature.properties.group) ? true : false
-          return {
-            ...feature,
-            properties: {
-              ...feature.properties,
-              show_on_map
-            }
-          }
-        })
-        setDataGeoJson(newGeoJson)
+  const handleSelectedInstitutions = (geographicLocationFilters: GeographicLocationFilter[]) => {
+    setSelectedGeographicLocationFilter(geographicLocationFilters)
+  }
+
+  const handleOnMarkerClick = (latlng: string | undefined) => {
+    console.log(`Map was clicked at ${latlng}}!`)
+    const filtered = documents.filter((doc) => doc.latlngStr === latlng)
+    setSelectedMarker(filtered)
+  }
+
+  useEffect(() => {
+    // Used to populate the filter options and set the initial selected filters
+
+    // TODO: replace with api call
+    const filterOptions = getFilterOptions()
+    setTreeData(filterOptions);
+
+    // set selected filters to all and pull markers in
+    const newSelectedGeographicLocationFilters: GeographicLocationFilter[] = []
+    filterOptions.forEach((category) => {
+      if (!category.Institutions) {
+        return
       }
+      category.Institutions.forEach((institution) => {
+        newSelectedGeographicLocationFilters.push({ Category: category.Category, Institution: institution })
+      })
+    });
+    setSelectedGeographicLocationFilter(newSelectedGeographicLocationFilters)
+  }, [])
 
-    const handleOnMarkerClick = (latlng: string | undefined) => {
-        console.log(`Map was clicked at ${latlng}}!`)
-        const filtered = documents.filter((doc) => doc.latlngStr === latlng)
-        setSelectedMarker(filtered)
-    }
+  useEffect(() => {
+    // Used to update markers shown on map when filters are changed
 
-    useEffect(() => {
-        const filterOptions = getFilterOptions()
-        const documents = getAllMapPoints();
-        const uniqueGeoJsonPreppedData = getUniqueGeoJsonPreppedPoints();
-        setDataGeoJson(uniqueGeoJsonPreppedData);
-        setTreeData(filterOptions);
-        setDocuments(documents);
-      }, [setDocuments, setTreeData, setDataGeoJson])
+    // TODO: replace with api call
+    const uniqueGeoJsonPreppedData = getUniqueGeoJsonPreppedPoints();
+    const newGeoJson = uniqueGeoJsonPreppedData.map((feature) => {
+      const show_on_map = selectedGeographicLocationFilters.find((option) => option.Institution === feature.properties.group) ? true : false
+      return {
+        ...feature,
+        properties: {
+          ...feature.properties,
+          show_on_map
+        }
+      }
+    })
+    setDataGeoJson(newGeoJson)
+  }, [selectedGeographicLocationFilters])
 
-    return {
-        selectedMarker,
-        dataGeoJson,
-        treeData,
-        documents,
-        handleCheckboxChange: handleSelectedInstitutions,
-        handleOnMarkerClick
-    }
+  useEffect(() => {
+    const documents = getAllMapPoints();
+    setDocuments(documents);
+  }, [])
+
+  return {
+    selectedMarker,
+    dataGeoJson,
+    treeData,
+    documents,
+    handleCheckboxChange: handleSelectedInstitutions,
+    handleOnMarkerClick
+  }
 }
 
 export default useLeafletMap;
