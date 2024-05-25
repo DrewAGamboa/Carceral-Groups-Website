0314843d75762db9c8a2f1208051967602719110
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-05-25 03:30:28 +0000

    remove unused types

--- carceral-groups-website/src/models/FiltersResponseFilter.ts
+++ carceral-groups-website/src/models/FiltersResponseFilter.ts
@@ -1,31 +1,6 @@
-export const DUMMY_FILTERS: FiltersResponseFilter[] = [
-    {
-        Category: "Federal",
-        Institutions: [
-            "Washington Corrections Center",
-        ]
-    },
-    {
-        Category: "State",
-        Institutions: [
-            "Evergreen Newsroom, Murrow Center",
-            "La Escuelita, Granger, WA"
-        ]
-    },
-]
-
 type FiltersResponseFilter = {
     Category: string,
     Institutions?: string[],
 }
-
-export type FilterOption = {
-    prefix: string,
-    label: string,
-    level: number,
-    checked: boolean
-    indeterminate: boolean
-}
-
 
 export default FiltersResponseFilter
