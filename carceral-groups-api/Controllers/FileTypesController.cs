6683cdf3227b1d9ba07cebec42caf9bde063665e
Cesar Lucatero <cesarlucatero@outlook.com>
2024-06-04 12:02:22 +0000

    sorts the document and file type lists returned for markers

--- carceral-groups-api/Controllers/FileTypesController.cs
+++ carceral-groups-api/Controllers/FileTypesController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using CarceralGroupsAPI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,11 +19,14 @@ namespace carceral_groups_api.Controllers
         [HttpGet]
         public async Task<FileType?[]> Get(int geographicLocationId)
         {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             var fileTypes = await _dbContext.Documents
                 .Where(m => m.GeographicLocationId == geographicLocationId)
                 .Select(m => m.FileType)
                 .Distinct()
+                .OrderBy(m => m.Name)
                 .ToArrayAsync();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
             return fileTypes;
         }
