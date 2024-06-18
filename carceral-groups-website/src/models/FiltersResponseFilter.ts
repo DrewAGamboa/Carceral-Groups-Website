66146708026a3ee0629af47c8f5eeef93d09b6d8
Vitaliy Alekhnovich <vitaliyalekhnovich@gmail.com>
2024-06-18 15:16:48 +0000

    Add custom color checkbox

--- carceral-groups-website/src/models/FiltersResponseFilter.ts
+++ carceral-groups-website/src/models/FiltersResponseFilter.ts
@@ -1,7 +1,9 @@
+// represents one category that has institutions
 type FiltersResponseFilter = {
     categoryId: string
     category: string,
     institutions?: {institutionId: string, institution: string}[],
+    color?: string
 }
 
 export default FiltersResponseFilter
